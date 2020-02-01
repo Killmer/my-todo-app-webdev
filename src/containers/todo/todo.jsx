c752dadb9a54886821fbb929b22fe6a59dbe0ba4
Dima Lytvyn <killmermail@gmail.com>
2020-02-01 21:26:27 +0200

    created repository

---
+++ src/containers/todo/todo.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addTask, removeTask, completeTask, changeFilter } from '../../actions/actionCreator';
+
+import ToDoInput from '../../components/todo-input/todo-input';
+import ToDoList from '../../components/todo-list/todo-list';
+import Footer from '../../components/footer/footer';
+
+import './todo.css';
+
+
+
+class ToDo extends Component {
+
+  state = {
+    taskText: '',
+  }
+
+  handleInputChange = ( { target: { value }} ) => {
+    this.setState({
+      taskText: value
+    })
+  }
+
+  filterTasks = (tasks, activeFilter) => {
+    switch (activeFilter) {
+      case 'completed':
+        return tasks.filter(task => task.isCompleted);
+        break;
+      case 'active':
+        return tasks.filter(task => !task.isCompleted);
+        break;
+      default:
+        return tasks;
+    }
+  }
+
+  getActiveTasksCounter = tasks => tasks.filter(task => !task.isCompleted).length
+
+  addTask = ({ key }) => {
+    const { taskText } = this.state;
+
+    if(taskText.length > 3 && key === 'Enter') {
+      const { addTask } = this.props;
+
+      addTask((new Date()).getTime(), taskText, false);
+
+      this.setState({
+        taskText: '',
+      })
+    }
+  }
+
+  render() {
+    const { taskText } = this.state;
+    const { tasks, removeTask, completeTask, filters, changeFilter } = this.props;
+    const isTasksExist = tasks && tasks.length > 0;
+    const filteredTasks = this.filterTasks(tasks, filters);
+    const taskCounter = this.getActiveTasksCounter(tasks);
+
+    return (
+      <div className="todo-wrapper">
+        <ToDoInput onKeyPress={this.addTask} onChange={this.handleInputChange} value={taskText}/>
+        {isTasksExist && <ToDoList tasksList={filteredTasks} removeTask={removeTask} completeTask={completeTask} />}
+        {isTasksExist && <Footer amount={taskCounter} activeFilter={filters} changeFilter={changeFilter}/>}
+      </div>
+    );
+  }
+}
+
+export default connect(({ tasks, filters }) => ({
+  tasks,
+  filters,
+}), { addTask, removeTask, completeTask, changeFilter })(ToDo);
